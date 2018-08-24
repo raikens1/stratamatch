24048211a864315538730edc2d9edd85ce5e30fc
raikens1 <rachaelaikens@gmail.com>
2018-08-24 12:37:21 -0700

    implemented auto_stratify

--- R/big_match.R
+++ R/big_match.R
@@ -1,3 +1,5 @@
+require("binr")
+
 
 #' @title Manually creates strata for matching
 #' @description Creates \code{strata} object, given a set of covariates to stratify on
@@ -31,17 +33,59 @@ manual_stratify <- function(data, treat, outcome, covariates){
 
 auto_stratify <- function(data, treat, outcome, covariates = NULL, prog_scores = NULL, size = 2000){
   
-  # check that exactly one of covariates or prog_scores is non-NULL
+  result <- list(data = NULL, prog_scores = NULL, prog_model = NULL)
   
-  # if prog_scores precalculated: check that they are valid
+  # check inputs
   
-  # else: build prognostic scores with a linear model
+  if (is.null(covariates) && is.null(prog_scores)){
+    stop("At least one of covariates and prog_scores should be specified.")
+  }
+  
+  if (!is.null(covariates) && !is.null(prog_scores)){
+    warning("covariates and prog_scores are both specified. Using prog_scores; ignoring covariates.") 
+  }
+  
+  if (!is.null(prog_scores)){
+    if (length(prog_scores) != dim(data)[1]){
+      stop("prog_scores must be the same length as the data")
+    } 
+  } else {
+    # if prog_scores are not specified, build them
+    
+    prog_model <- build_prog_model(data, treat, outcome, covariates)
+    prog_scores <- predict(prog_model, lalonde, type = "response")
+    
+    result$prog_model <- prog_model
+    
+  }
   
   # Create strata from prognostic score quantiles
+  n_bins <- ceiling(dim(data)[1]/size)
+  data$stratum <- ntile(prog_scores, n_bins)
   
-  # If prognostic score is not continuous, handle that.
+  # package and resturn result
+  result$data = data
+  result$prog_scores = prog_scores
+  class(result) <- "strata"
   
-  # return strata object
+  return(result)
+}
+
+#' @title Generates prognostic score model from the data
+#' @description When the prog_score is not precalculated, fits a prognostic score model to the data according to specifications.  Currently just fits a logistic model on controls.
+#' @param data data frame with observations as rows, features as columns
+#' @param treat string giving the name of column designating treatment assignment
+#' @param outcome string giving the name of column with outcome information
+#' @param covariates a vector of the columns to be used as covariates building prognostic score model
+#' @return Returns a \code{strata} object
+
+build_prog_model <- function(data, treat, outcome, covariates){
+  formula_str <- paste(outcome, paste(covariates, collapse ="+"), sep = "~")
   
-  return(0)
+  # fit model on controls only
+  data0 <- subset(data, treat == 0)
+  
+  model <- glm(formula(formula_str), data = data0, family = "binomial")
+  
+  return(model)
 }
