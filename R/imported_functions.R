f7a0785ecf8549803b2869f931f7be68779f0d38
raikens1 <rockyaikens@gmail.com>
2019-08-29 14:23:53 -0700

    fixed namespace issue for strata from survival package

---
+++ R/imported_functions.R
@@ -0,0 +1,19 @@
+#' Pipe operator
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
+
+#' Strata function from package Survival
+#'
+#' @name strata
+#' @rdname strata
+#' @keywords internal
+#' @export
+#' @importFrom survival strata
+#' @usage strata(..., na.group = FALSE, shortlabel, sep = ", ")
+NULL
