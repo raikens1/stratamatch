44fb6887ecc37c982a8469a08d7d4642e6ecc61b
raikens1 <rockyaikens@gmail.com>
2019-09-05 11:34:59 -0700

    removed multidplyr

--- R/match_functions.R
+++ R/match_functions.R
@@ -86,67 +86,6 @@ big_match_dopar <- function(strat, propensity_formula = NULL, k = 1) {
 }
 
 
-#' Big Match Multidplyr
-#'
-#' Still in development. Match within strata in parallel by calling match_one
-#' with multidplyr.  Currently buggy, but returns a result.
-#'
-#' @inheritParams big_match
-#' 
-#' @return a data.frame like dat with pair assignments
-big_match_multidplyr <- function(strat, propensity_formula = NULL, k = 1) {
-
-  check_inputs_matcher(strat, propensity_formula, k)
-
-  t1 <- proc.time()
-  if (is.null(propensity_formula)){
-    propensity_formula <- formula(paste(c(strat$treat, "~ . -", strat$outcome,
-                                          "- stratum"), collapse = ""))
-  }
-
-  print(propensity_formula)
-
-  # build propensity model
-  propensity_model <- glm(propensity_formula,
-                          data = strat$analysis_set,
-                          family = binomial())
-
-  # set up multidplyr cluster
-  num_cores <- parallel::detectCores()
-  treat <- strat$treat
-  cluster <- multidplyr::new_cluster(num_cores)
-  multidplyr::cluster_assign(cluster,
-                             propensity_model = propensity_model,
-                             match_on = optmatch::match_on,
-                             pairmatch = optmatch::pairmatch,
-                             match_one = match_one,
-                             make_distance_matrix = make_distance_matrix,
-                             treat = treat,
-                             k = k)
-  # match in parallel
-  #result <- strat$analysis_set %>%
-  #  group_by(stratum) %>%
-  #  partition(cluster = cluster) %>%
-  #  do(match_one(., propensity_model = propensity_model, treat = treat, k = k)) %>% collect()
-
-  stp1 <- dplyr::group_by(strat$analysis_set, stratum)
-
-  stp2 <- multidplyr::partition(stp1, cluster = cluster)
-
-  print(is(stp2))
-  print(methods(do))
-
-  stp3 <- do(stp2, match_one(.,
-                             propensity_model = propensity_model,
-                             treat = treat,
-                             k = k))
-
-  result <- multidplyr::collect(stp3)
-
-  #parallel::stopCluster(cluster)
-
-  return(result)
-}
 
 #' Big Match
 #'
